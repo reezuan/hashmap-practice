8136c748943dd05d568186100cadf5f5ce37c12d
Rizuan <john.lee.coco@gmail.com>
2024-06-21 18:56:34 +0800

    Create HashMap class

--- HashMap.js
+++ HashMap.js
@@ -1,3 +1,23 @@
+import { LinkedList } from "./LinkedList.js";
+
 class HashMap {
+
+    constructor(capacity) {
+        this._buckets = Array.from({length: capacity}, () => new LinkedList());
+    }
+    
+    // Takes a key and produces a hash code with it.
+    hash(key) {
+        let hashCode = 0;
+                
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % this._buckets.length;
+        }
+        
+        return hashCode;
+    }
+
     
 }
