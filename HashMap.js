168090d901c56a71da1f57df50bbb7f737a16640
Rizuan <john.lee.coco@gmail.com>
2024-06-23 17:26:28 +0800

    Add method to insert item into hash map

--- HashMap.js
+++ HashMap.js
@@ -4,6 +4,8 @@ class HashMap {
 
     constructor(capacity) {
         this._buckets = Array.from({length: capacity}, () => new LinkedList());
+        this._loadFactor = 0.75;
+        this._maxBuckets = capacity * this._loadFactor;
     }
     
     // Takes a key and produces a hash code with it.
@@ -19,5 +21,15 @@ class HashMap {
         return hashCode;
     }
 
-    
+    // Takes a key and a value that is assigned to this key. If a key
+    // already exists, then the old value is overwritten.
+    set(key, value) {
+        const hashCode = hash(key);
+
+        if (this._buckets[hashCode].size() === 0 || !this._buckets[hashCode].contains(key)) {
+            this._buckets[hashCode].prepend(key, value, hashCode);
+        } else if (this._buckets[hashCode].contains(key)) {
+            this._buckets[hashCode].update(key, value);
+        }
+    }
 }
